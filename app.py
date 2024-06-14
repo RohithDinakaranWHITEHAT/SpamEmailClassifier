82c2b49f54e9fd33a9a095dcd49c1be095a739e2
Rohith Dinakaran <61183902+RohithDinakaranWHITEHAT@users.noreply.github.com>
2024-06-14 09:53:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
 
-nltk.download('stopwords')
+
 
 # Load the saved model
 model = load_model('spam_classifier_model.h5')
@@ -22,7 +22,7 @@ def preprocess_text(text):
     text = text.translate(str.maketrans('', '', string.punctuation))
     words = text.split()
     stemmer = PorterStemmer()
-    stopwords_set = set(stopwords.words('english'))
+    stopwords_set = set(["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"])
     words = [stemmer.stem(word) for word in words if word not in stopwords_set]
     return ' '.join(words)
 
